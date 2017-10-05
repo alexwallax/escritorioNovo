c89b5fdaa8fcc5cb0941cdc207468064dd62225c
alexwallax <31941706+alexwallax@users.noreply.github.com>
2017-10-04 22:16:25 -0300

    conserto no pom

--- src/main/java/com/rcelestinoadvocacia/contrtoller/LoginBean.java
+++ src/main/java/com/rcelestinoadvocacia/contrtoller/LoginBean.java
@@ -1,51 +1,6 @@
 package com.rcelestinoadvocacia.contrtoller;
 
-import com.rcelestinoadvocacia.dao.LoginDAO;
 
-import com.rcelestinoadvocacia.model.Login;
-
-import java.util.List;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-@ViewScoped
-@ManagedBean(name = "LoginBean")// informa para o JSF � um gerenciador de telas
 public class LoginBean {
-    
-    private Login login = new Login(); 
-
-    
-    public void salvar(){
-    LoginDAO dao = new LoginDAO();
-    dao.insert(this.login);
-    this.login = new Login();
-    }
-    
-    public void editar(Login login){
-    LoginDAO dao = new LoginDAO();
-    dao.update(login);
-    }
-
-    public void remover(Login login){
-    LoginDAO dao = new LoginDAO();
-    dao.delete(login); 
-    }
-    
-    public List<Login> getLogins(){
-    LoginDAO dao = new LoginDAO();
-    return dao.selectAll();
-    }   
-    
-
-    public LoginBean() {
-    }
-          
-    public Login getLogin() {
-        return login;
-    }
-
-    public void setLogin(Login login) {
-        this.login = login;
-    }
-
     
 }
